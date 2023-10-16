1f38551a5446d0988290ceb8456506c72fe2013d
ELIF HELIN CARBOGA <elifhelincarboga@gmail.com>
2023-10-16 17:20:27 +0300

    Aktif ödev 3 eklendi

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import React, { useState } from 'react'
+import './App.css';
+
+function App() {
+    let [width, setWidth] = useState(200);
+    let [height, setHeight] = useState(200);
+
+    const change = () => {
+      debugger
+      setWidth(width)
+      setHeight(height)
+    }
+
+    const changeWidth = (e) => {
+      width = e
+    }
+
+    const changeHeight = (e) => {
+      height = e
+    }
+
+    return (<>
+        <div>
+          <label htmlFor="">Width: </label>
+          <input type='text' onChange={(e) => changeWidth(e.target.value)} />
+        </div>
+        <div>
+          <label htmlFor="">Height: </label>
+          <input type='text' onChange={(e) => changeHeight(e.target.value)} />
+        </div>
+        <div style={{ width: width, height: height, border: "1px solid red" }}></div>
+        <div>
+            <button onClick={() => change()}>Change</button>
+        </div>
+    </>
+    )
+}
+
+export default App;
